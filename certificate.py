33f7016e7829f1867139c0c6fa21ae56031841a8
AdixSasuke <adityakhandelwal0271@gmail.com>
2024-11-10 03:41:48 +0530

    minor additions

--- certificate.py
+++ certificate.py
@@ -4,8 +4,8 @@ import os
 
 data = pd.read_excel('participants.xlsx')
 
-template_path = 'certificate_template.png'
-font_path = 'MonsieurLaDoulaise-Regular.ttf'
+template_path = 'template.png'  # Change templateName here
+font_path = 'MonsieurLaDoulaise-Regular.ttf'  # Change font here
 output_folder = 'certificates'
 
 if not os.path.exists(output_folder):
@@ -24,12 +24,13 @@ for index, row in data.iterrows():
     text_bbox = draw.textbbox((0, 0), text, font=font_name)
     text_width = text_bbox[2] - text_bbox[0]
 
-    x_name = (certificate_width - text_width) / 2
-    y_name = 720
+    x_pos = (certificate_width - text_width) / 2
+    y_pos = 720  # you can adjust height here
 
-    draw.text((x_name, y_name), text, fill="black", font=font_name)
+    draw.text((x_pos, y_pos), text, fill="black", font=font_name)
 
-    output_path = os.path.join(output_folder, f"{row['Name']}_certificate.png")
+    first_name = row['Name'].split()[0]
+    output_path = os.path.join(output_folder, f"{first_name}_certificate.png")  # Change exported fileName here
     certificate.save(output_path)
 
 print('Certificates Generated')
